707d84e9264d8f0314a3920e6e03769ce3732f01
timurche <you@example.com>
2022-12-23 16:55:25 +0500

    create product

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,8 +1,9 @@
+const db = require("../db")
 class ProductController {
 	async createProduct(req, res) {
 		const { pname, price } = req.body
-		console.log(pname, price)
-		res.json('ok')
+		const newProduct = await db.query(`INSERT INTO PRODUCTS (pname, price) VALUES ($1, $2) RETURNING *`, [pname,price])
+		res.json(newProduct)
 	}
 	async getProducts(req, res) {
 
