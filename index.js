dedb1bd33ae9c7f3aba212e02046cdd7e60e4597
timurche <you@example.com>
2022-12-23 16:01:08 +0500

    json

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const router = require('./routes/product.routes')
 const PORT = process.env.PORT || 8080
 
 const app = express()
-app.use(express.json)
+app.use(express.json())
 app.use('/api', router)
 app.listen(PORT, () => { console.log(`app started on ${PORT}`) })
